dfe53425e54e3f2990495b002eadc0a4be4a10da
Jinx <chisel.delahanty@gmail.com>
2025-03-16 18:31:53 +0100

    Get user input, lowercase it, and reset UI

--- script.js
+++ script.js
@@ -2,3 +2,26 @@
 // Clean user input received from search box: lowercase it
 // If user input includes any characters other than a-z and hyphen, return "not found" message
 // Else if user input corresponds to a pokemon, show it
+
+// UI variables
+const userInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+// const userInput = document.getElementById();
+// const userInput = document.getElementById();
+// const userInput = document.getElementById();
+// const userInput = document.getElementById();
+
+const getUserInput = (input) => {
+	const lowerCaseInput = input.toLowerCase();
+	console.log(lowerCaseInput);
+	return lowerCaseInput;
+}
+
+const reset = () => {
+	document.getElementById("search-input").value = "";
+}
+
+searchButton.addEventListener("click", () => {
+	getUserInput(userInput.value);
+	reset();
+});
