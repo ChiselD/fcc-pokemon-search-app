226eb83ea812de1e36e5da313e8b9c6092d3db0b
Jinx <chisel.delahanty@gmail.com>
2025-03-16 20:56:19 +0100

    Final touches (HTML and JS now complete)

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const speedDisplay = document.getElementById("speed");
 
 const reset = () => {
 	document.getElementById("search-input").value = "";
+	typesDisplay.innerHTML = "";
 }
 
 // search for queried Pokemon
@@ -25,13 +26,14 @@ async function getPokemonArr(searchQuery) {
 	try {
 		const res = await fetch(url);
 		if (!res.ok) {
-			alert("Pokemon not found.");
+			alert("Pokémon not found");
 			throw new Error(`Error! Response status: ${res.status}`);
+			return 1;
 		}
 		const pokemonDetails = await res.json();
 		console.log(pokemonDetails);
 		if (!pokemonDetails) {
-			console.log("Not found");
+			alert("Pokémon not found");
 			return 1;
 		}
 		displayPokemon(pokemonDetails);
@@ -44,32 +46,19 @@ async function getPokemonArr(searchQuery) {
 
 // display details on requested Pokemon
 const displayPokemon = (pokemon) => {
-	console.log(`Pokemon name: ${pokemon.name.toUpperCase()}`);
 	nameDisplay.textContent = pokemon.name.toUpperCase();
-	console.log(`Pokemon ID: #${pokemon.id}`);
 	idDisplay.textContent = `#${pokemon.id}`;
-	console.log(`Pokemon weight: ${pokemon.weight}`);
 	weightDisplay.textContent = pokemon.weight;
-	console.log(`Pokemon height: ${pokemon.height}`);
 	heightDisplay.textContent = pokemon.height;
 	spriteDisplay.src = pokemon.sprites.front_default;
-	const types = [];
 	for (let i = 0; i < pokemon.types.length; i++) {
-		types.push(pokemon.types[i].type.name);
+		typesDisplay.innerHTML += `<span>${pokemon.types[i].type.name.toUpperCase()}</span>`;
 	}
-	console.log("Pokemon types: " + types.join(", "));
-	typesDisplay.textContent = types.join(", ");
-	console.log(`Pokemon HP: ${pokemon.stats[0].base_stat}`);
 	hpDisplay.textContent = pokemon.stats[0].base_stat;
-	console.log(`Pokemon attack: ${pokemon.stats[1].base_stat}`);
 	attackDisplay.textContent = pokemon.stats[1].base_stat;
-	console.log(`Pokemon defense: ${pokemon.stats[2].base_stat}`);
 	defenseDisplay.textContent = pokemon.stats[2].base_stat;
-	console.log(`Pokemon special attack: ${pokemon.stats[3].base_stat}`);
 	specialAttackDisplay.textContent = pokemon.stats[3].base_stat;
-	console.log(`Pokemon special defense: ${pokemon.stats[4].base_stat}`);
 	specialDefenseDisplay.textContent = pokemon.stats[4].base_stat;
-	console.log(`Pokemon speed: ${pokemon.stats[5].base_stat}`);
 	speedDisplay.textContent = pokemon.stats[5].base_stat;
 }
 
