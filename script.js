eeeefddc98be9b5289586bc3f450d217c98089f5
Jinx <chisel.delahanty@gmail.com>
2025-03-16 14:53:58 +0100

    Start writing pseudocode for JS

---
+++ script.js
@@ -0,0 +1,4 @@
+// PSEUDOCODE
+// Clean user input received from search box: lowercase it
+// If user input includes any characters other than a-z and hyphen, return "not found" message
+// Else if user input corresponds to a pokemon, show it
