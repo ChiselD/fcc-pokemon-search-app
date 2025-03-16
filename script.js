2b0997f5835a4401dee1e8c91d4fae4c44fc4588
Jinx <chisel.delahanty@gmail.com>
2025-03-16 18:40:32 +0100

    Set up async function to get Pokemon array

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 // Else if user input corresponds to a pokemon, show it
 
 // UI variables
+
 const userInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 // const userInput = document.getElementById();
@@ -11,9 +12,27 @@ const searchButton = document.getElementById("search-button");
 // const userInput = document.getElementById();
 // const userInput = document.getElementById();
 
+// other variables
+
+// array of all pokemon objects
+async function getPokemonArr() {
+	const url = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+	try {
+		const res = await fetch(url);
+		if (!res.ok) {
+			throw new Error(`Error! Response status: ${res.status}`);
+		}
+		const json = await res.json();
+		// console.log(json);
+		console.log(json.results[0].name);
+	} catch (err) {
+		console.error(err.message);
+	}
+}
+
 const getUserInput = (input) => {
 	const lowerCaseInput = input.toLowerCase();
-	console.log(lowerCaseInput);
+	// console.log(lowerCaseInput);
 	return lowerCaseInput;
 }
 
@@ -23,5 +42,6 @@ const reset = () => {
 
 searchButton.addEventListener("click", () => {
 	getUserInput(userInput.value);
+	getPokemonArr();
 	reset();
 });
